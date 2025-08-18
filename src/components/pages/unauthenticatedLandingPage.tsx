beec771a5c21b9972987bc5ef0ac380a5667f2ed
dharmendra007 <mahantas025@gmail.com>
2025-08-18 18:03:45 +0530

    add favicon

--- src/components/pages/unauthenticatedLandingPage.tsx
+++ src/components/pages/unauthenticatedLandingPage.tsx
@@ -12,7 +12,7 @@ export default function UnauthenticatedLandingPage() {
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
               </svg>
             </div>
-            <span className="text-2xl font-bold text-white">Formly</span>
+            <span className="text-2xl font-bold text-white">Form Generator</span>
           </Link>
 
           <div className="flex items-center space-x-5">
